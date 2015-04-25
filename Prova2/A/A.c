f7f98f5d7b583fa020a5d6071545142b638ff269
João Nobre <joaoanobre@gmail.com>
2015-04-25 17:46:54 +0100

    Ex A Prova2 Accepted

--- Prova2/A/A.c
+++ Prova2/A/A.c
@@ -12,26 +12,11 @@ typedef struct strt_evento
 }reuniao;
 
 reuniao reunioes[MAX];
-int inicios[MAX],
-	fim[MAX],
-	conta,
+int conta,
 	num_reunioes;
 
+
 int ordena_asc(const void * a, const void * b)
-{
-	const int *p1 = (const int *)a,
-				 *p2 = (const int *)b;
-
-	if(*p1 == *p2)
-    	return 0;
- 	else
-	    if(*p1 < *p2)
-	    	return -1;
-	    else
-	    	return 1;
-}
-
-int ordena_desc(const void * a, const void * b)
 {
 	const reuniao *p1 = (const reuniao *)a,
 				 *p2 = (const reuniao *)b;
@@ -76,11 +61,10 @@ int main()
 		{
 			reunioes[i].verificado = 48;
 			scanf("%d %d",&reunioes[i].inicio,&reunioes[i].fim);			
-		/*	scanf("%d %d",&inicios[i],&fim[i]);*/
 		}
 		conta = 0;
 
-		qsort(reunioes,num_reunioes,sizeof(reuniao),ordena_desc);
+		qsort(reunioes,num_reunioes,sizeof(reuniao),ordena_asc);
 		calc();
 		printf("%d\n",conta);
 	}
